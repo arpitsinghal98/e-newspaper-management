edaa10b7c6cc7874ddffdf2a2689f8884c6cb5cb
Arpit Singhal <singhal.arpit@hotmail.com>
2024-11-16 12:43:16 -0600

    removed all non used items

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -4,43 +4,6 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:9000/api';
 
-// Articles API
-export const getArticles = (page = 1, limit = 10) => {
-  return axios.get(`${API_URL}/articles`, {
-    params: { page, limit },
-  });
-};
-
-export const getArticle = (id) => axios.get(`${API_URL}/articles/${id}`);
-export const createArticle = (data) => axios.post(`${API_URL}/articles`, data);
-export const updateArticle = (id, data) => axios.put(`${API_URL}/articles/${id}`, data);
-export const deleteArticle = (id) => axios.delete(`${API_URL}/articles/${id}`);
-
-// Categories API
-export const getCategories = (page = 1, limit = 10) => {
-  return axios.get(`${API_URL}/categories`, {
-    params: { page, limit },
-  });
-};
-
-export const getCategory = (id) => axios.get(`${API_URL}/categories/${id}`);
-export const createCategory = (data) => axios.post(`${API_URL}/categories`, data);
-export const updateCategory = (id, data) => axios.put(`${API_URL}/categories/${id}`, data);
-export const deleteCategory = (id) => axios.delete(`${API_URL}/categories/${id}`);
-
-// Comments API
-export const getComments = (page = 1, limit = 10) => {
-  return axios.get(`${API_URL}/comments`, {
-    params: { page, limit },
-  });
-};
-
-export const getComment = (id) => axios.get(`${API_URL}/comments/${id}`);
-export const createComment = (data) => axios.post(`${API_URL}/comments`, data);
-export const updateComment = (id, data) => axios.put(`${API_URL}/comments/${id}`, data);
-export const deleteComment = (id) => axios.delete(`${API_URL}/comments/${id}`);
-
-// Fetch Table Names
 export const getTables = () => {
   return axios.get(`${API_URL}/tables`); // Assuming you have an endpoint `/api/tables` in your backend
 };
